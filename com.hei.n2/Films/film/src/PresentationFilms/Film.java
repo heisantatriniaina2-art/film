7392bb285c5320961f86e6d96da031383f8617cc
RAKOTONINDRINA Antsalalaina Fy <hei.antsa.5@gmail.com>
2026-06-20 14:07:51 +0300

    ajout des méthodes dans Administrateur et Client, et changement dans Contenu

--- com.hei.n2/Films/film/src/PresentationFilms/Film.java
+++ com.hei.n2/Films/film/src/PresentationFilms/Film.java
@@ -5,11 +5,9 @@ import java.time.LocalTime;
 public class Film extends Contenu{
     private LocalTime duree;
 
-    public Film(int idContenu, GenreFilm genreFilm, String nomFilm, LocalTime duree){
-        super(idContenu, genreFilm, nomFilm);
+    public Film(int idContenu, GenreFilm genreFilm, String titre, int popularite, LocalTime duree){
+        super(idContenu, genreFilm, titre, popularite);
         this.duree = duree;
     }
-
-
 
 }
--- com.hei.n2/Films/film/src/PresentationFilms/Serie.java
+++ com.hei.n2/Films/film/src/PresentationFilms/Serie.java
@@ -8,8 +8,8 @@ public class Serie extends Contenu {
     private int nombreEpisode;
     private LocalTime dureeEpisode;
 
-  public Serie (int idContenu, GenreFilm genreFilm, String nomFilm, int nombreEpisode, int nombreSaison, LocalTime dureeEpisode){
-      super(idContenu, genreFilm, nomFilm);
+  public Serie (int idContenu, GenreFilm genreFilm, String titre,int popularite, int nombreEpisode, int nombreSaison, LocalTime dureeEpisode){
+      super(idContenu, genreFilm, titre, popularite);
       this.nombreEpisode = nombreEpisode;
       this.nombreSaison = nombreSaison;
       this.dureeEpisode = dureeEpisode;
