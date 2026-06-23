c09fac7a7d6d1f06db2ba8073ab67b881596aa6a
heimampitony12 <hei.mampitony@gmail.com>
2026-06-23 11:14:19 +0300

    Voici dans le dossier origine les modifications que j'ai ajouter dans la classe client et clientTest

--- com.hei.n2/Films/film/src/PresentationFilms/Client.java
+++ com.hei.n2/Films/film/src/PresentationFilms/Client.java
@@ -2,6 +2,7 @@
 package PresentationFilms;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Client extends Utilisateur {
     public Abonnement abonnement;
@@ -9,7 +10,6 @@ public class Client extends Utilisateur {
     public Client(int idUtilisateur, String nomUtilisateur, String prenomUtilisateur, String email, LocalDate dateNaissance){
         super(idUtilisateur,nomUtilisateur, prenomUtilisateur, email, dateNaissance);
     }
-
 
     public void setAbonnement(Abonnement abonnement) {
         this.abonnement = abonnement;
@@ -23,4 +23,30 @@ public class Client extends Utilisateur {
         System.out.println(getNomUtilisateur() + " regarde le média : " + media.getTitre());
         media.incrementePopularite();
     }
+
+    public List<Film> chercherFilms(List<Film> tousFilms, String titreRecherche) {
+        List<Film> resultats = tousFilms.stream()
+                .filter(film -> film.getTitre().toLowerCase().contains(titreRecherche.toLowerCase()))
+                .toList();
+
+        System.out.println("Résultats de la recherche par titre :");
+
+        resultats.forEach(film -> System.out.println("Titre : " + film.getTitre() + ", Genre : " + film.getGenreFilm()));
+
+        return resultats;
+    }
+
+    public List<Film> chercherFilmsByGenre(List<Film> tousFilms, GenreFilm genre) {
+        List<Film> resultats = tousFilms.stream()
+                .filter(film -> film.getGenreFilm() == genre)
+                .toList();
+
+        System.out.println("Résultats de la recherche par genre :");
+
+        resultats.forEach(film -> System.out.println("Titre : " + film.getTitre() + ", Genre : " + film.getGenreFilm()));
+
+        return resultats;
+    }
+
+
 }
