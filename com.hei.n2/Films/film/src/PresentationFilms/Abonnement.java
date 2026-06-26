0fc622d1a1bc24230b8d31cb85b2d13fbd4e76c1
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-06-26 18:25:46 +0300

    J'ai modifié un peu la classe Abonnement

--- com.hei.n2/Films/film/src/PresentationFilms/Abonnement.java
+++ com.hei.n2/Films/film/src/PresentationFilms/Abonnement.java
@@ -3,15 +3,35 @@ import PresentationFilms.TypeAbonnement;
 
 public class Abonnement {
     private double prix;
+    private int idAbonnement;
+    private TypeAbonnement typeAbonnement;
 
-    public Abonnement(int idAbonnement, TypeAbonnement type) {
+    public Abonnement(int idAbonnement, double prix, TypeAbonnement type) {
 
         if (type == TypeAbonnement.PREMIUM) this.prix = 15.0;
         else if (type == TypeAbonnement.VIP) this.prix = 15.0;
         else this.prix = 0.0;
+        this.idAbonnement = idAbonnement;
     }
 
     public double getPrix() {
         return this.prix;
+    }
+
+    public int getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public void setIdAbonnement(int idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    @Override
+    public String toString() {
+        return "Abonnement ID: " + idAbonnement + " | Type: " + typeAbonnement + " | Prix: " + getPrix() + "Ariary";
     }
 }
--- com.hei.n2/Films/film/src/PresentationFilms/Utilisateur.java
+++ com.hei.n2/Films/film/src/PresentationFilms/Utilisateur.java
@@ -17,6 +17,7 @@ public abstract class Utilisateur {
         this.dateNaissance = dateNaissance;
     }
 
+
     public int getIdUtilisateur() {
         return idUtilisateur;
     }
