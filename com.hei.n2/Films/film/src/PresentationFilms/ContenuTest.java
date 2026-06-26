29194018d715cbb08d78f02d62aa47bbda02d46e
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-06-26 21:29:11 +0300

    projet film (diagramme)

--- com.hei.n2/Films/film/src/PresentationFilms/ContenuTest.java
+++ com.hei.n2/Films/film/src/PresentationFilms/ContenuTest.java
@@ -16,7 +16,13 @@ public class ContenuTest {
         var futurFilm = new Film(2, GenreFilm.ACTION, "Prochain Hit", 85, LocalTime.of(1,53 ));
         assertFalse(futurFilm.estDisponible(), "Le film ne devrait pas être disponible");
     }
-
     @Test
-    void test
+    void testAjouterAvis() {
+        Film film = new Film(101, GenreFilm.ACTION, "Transporteur", 85, LocalTime.of(1, 28, 45));
+        Avis avis = new Avis("UtilisateurTest", 5, "Super film !");
+        film.ajouterAvis(avis);
+
+        assertEquals(1, film.getListeAvis().size(), "La liste d'avis devrait contenir 1 élément.");
+        assertEquals("UtilisateurTest", film.getListeAvis().get(0).getNomUtilisateur());
+    }
 }
